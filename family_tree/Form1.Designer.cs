6bdf26a00bad5b4d9ea5e474dbc69ecf73140152
musauysal033 <33937971+musauysal033@users.noreply.github.com>
2020-09-01 21:58:55 +0300

    Add project files.

---
+++ family_tree/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace family_tree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxAD = new System.Windows.Forms.TextBox();
+            this.textBoxsoyad = new System.Windows.Forms.TextBox();
+            this.textBoxbabaad = new System.Windows.Forms.TextBox();
+            this.textBoxbbsoyad = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxannead = new System.Windows.Forms.TextBox();
+            this.textBoxannesoyad = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Location = new System.Drawing.Point(16, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label2.Location = new System.Drawing.Point(16, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label3.Location = new System.Drawing.Point(16, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Baba Adı:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label4.Location = new System.Drawing.Point(16, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 20);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Baba Soyadı:";
+            // 
+            // textBoxAD
+            // 
+            this.textBoxAD.Location = new System.Drawing.Point(137, 15);
+            this.textBoxAD.Name = "textBoxAD";
+            this.textBoxAD.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAD.TabIndex = 2;
+            // 
+            // textBoxsoyad
+            // 
+            this.textBoxsoyad.Location = new System.Drawing.Point(137, 45);
+            this.textBoxsoyad.Name = "textBoxsoyad";
+            this.textBoxsoyad.Size = new System.Drawing.Size(100, 20);
+            this.textBoxsoyad.TabIndex = 2;
+            // 
+            // textBoxbabaad
+            // 
+            this.textBoxbabaad.Location = new System.Drawing.Point(137, 78);
+            this.textBoxbabaad.Name = "textBoxbabaad";
+            this.textBoxbabaad.Size = new System.Drawing.Size(100, 20);
+            this.textBoxbabaad.TabIndex = 2;
+            // 
+            // textBoxbbsoyad
+            // 
+            this.textBoxbbsoyad.Location = new System.Drawing.Point(137, 107);
+            this.textBoxbbsoyad.Name = "textBoxbbsoyad";
+            this.textBoxbbsoyad.Size = new System.Drawing.Size(100, 20);
+            this.textBoxbbsoyad.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label5.Location = new System.Drawing.Point(20, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 19);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Anne Ad:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label6.Location = new System.Drawing.Point(20, 174);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(112, 19);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Anne Soyadı:";
+            // 
+            // textBoxannead
+            // 
+            this.textBoxannead.Location = new System.Drawing.Point(137, 140);
+            this.textBoxannead.Name = "textBoxannead";
+            this.textBoxannead.Size = new System.Drawing.Size(100, 20);
+            this.textBoxannead.TabIndex = 2;
+            // 
+            // textBoxannesoyad
+            // 
+            this.textBoxannesoyad.Location = new System.Drawing.Point(137, 175);
+            this.textBoxannesoyad.Name = "textBoxannesoyad";
+            this.textBoxannesoyad.Size = new System.Drawing.Size(100, 20);
+            this.textBoxannesoyad.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(292, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 39);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 213);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(526, 495);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(550, 720);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxannesoyad);
+            this.Controls.Add(this.textBoxannead);
+            this.Controls.Add(this.textBoxbbsoyad);
+            this.Controls.Add(this.textBoxbabaad);
+            this.Controls.Add(this.textBoxsoyad);
+            this.Controls.Add(this.textBoxAD);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Family Tree";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxAD;
+        private System.Windows.Forms.TextBox textBoxsoyad;
+        private System.Windows.Forms.TextBox textBoxbabaad;
+        private System.Windows.Forms.TextBox textBoxbbsoyad;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxannead;
+        private System.Windows.Forms.TextBox textBoxannesoyad;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ family_tree/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace family_tree
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string yol = "Data source=dbapps.db";
+            SQLiteConnection con = new SQLiteConnection(yol);
+            con.Open();
+            string sql = "insert into Person(Ad,Soyad,Babaad,Babasoyad,Annead,Annesoyad) values(@Ad,@Soyad,@Babaad,@Babasoyad,@annead,@Annesoyad)";
+            SQLiteCommand cmd = new SQLiteCommand(sql, con);
+            cmd.Parameters.AddWithValue("@Ad", textBoxAD.Text);
+            cmd.Parameters.AddWithValue("@Soyad", textBoxsoyad.Text);
+            cmd.Parameters.AddWithValue("@Babaad", textBoxbabaad.Text);
+            cmd.Parameters.AddWithValue("@Babasoyad", textBoxbbsoyad.Text);
+            cmd.Parameters.AddWithValue("@Annead", textBoxannead.Text);
+            cmd.Parameters.AddWithValue("@Annesoyad", textBoxannesoyad.Text);
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Kayıt basarılı");
+        }
+    }
+}
