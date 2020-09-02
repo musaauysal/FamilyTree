4d32f8c206bfac761bcb90c6e3f1eab42e6f4949
musauysal033 <33937971+musauysal033@users.noreply.github.com>
2020-09-02 10:37:00 +0300

    Musa
    Bos giris engellendi

--- family_tree/Form1.cs
+++ family_tree/Form1.cs
@@ -20,19 +20,29 @@ namespace family_tree
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string yol = "Data source=dbapps.db";
-            SQLiteConnection con = new SQLiteConnection(yol);
-            con.Open();
-            string sql = "insert into Person(Ad,Soyad,Babaad,Babasoyad,Annead,Annesoyad) values(@Ad,@Soyad,@Babaad,@Babasoyad,@annead,@Annesoyad)";
-            SQLiteCommand cmd = new SQLiteCommand(sql, con);
-            cmd.Parameters.AddWithValue("@Ad", textBoxAD.Text);
-            cmd.Parameters.AddWithValue("@Soyad", textBoxsoyad.Text);
-            cmd.Parameters.AddWithValue("@Babaad", textBoxbabaad.Text);
-            cmd.Parameters.AddWithValue("@Babasoyad", textBoxbbsoyad.Text);
-            cmd.Parameters.AddWithValue("@Annead", textBoxannead.Text);
-            cmd.Parameters.AddWithValue("@Annesoyad", textBoxannesoyad.Text);
-            cmd.ExecuteNonQuery();
-            MessageBox.Show("Kayıt basarılı");
+            if (textBoxAD.Text == "" || textBoxsoyad.Text == "" || textBoxbabaad.Text == "" || textBoxbbsoyad.Text == "" || textBoxannead.Text == "" || textBoxannesoyad.Text == "") {
+                MessageBox.Show("Lütfen bilgileri eksiksiz girin...");
+            }
+            else
+            {
+                string yol = "Data source=dbapps.db";
+                SQLiteConnection con = new SQLiteConnection(yol);
+                con.Open();
+                string sql = "insert into Person(Ad,Soyad,Babaad,Babasoyad,Annead,Annesoyad) values(@Ad,@Soyad,@Babaad,@Babasoyad,@annead,@Annesoyad)";
+                SQLiteCommand cmd = new SQLiteCommand(sql, con);
+                cmd.Parameters.AddWithValue("@Ad", textBoxAD.Text);
+                cmd.Parameters.AddWithValue("@Soyad", textBoxsoyad.Text);
+                cmd.Parameters.AddWithValue("@Babaad", textBoxbabaad.Text);
+                cmd.Parameters.AddWithValue("@Babasoyad", textBoxbbsoyad.Text);
+                cmd.Parameters.AddWithValue("@Annead", textBoxannead.Text);
+                cmd.Parameters.AddWithValue("@Annesoyad", textBoxannesoyad.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Veriler Kaydedildi...");
+            }
+
+
+
+            
         }
     }
 }
